0b95a34c9eae973dde7fdcb94c56d9aaf2764402
Hannah Deters <hannah@spreedly.com>
2019-06-28 12:32:44 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :flights, only: [:index]
   resources :transactions, only: [:index, :create]
+  resources :cards, only: [:destroy]
  
   root "flights#index"
 end
